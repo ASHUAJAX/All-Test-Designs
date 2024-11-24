3331d74b47f6faddc4c30439e59144df1f13450c
Ashutosh Jha <yesaj9354@gmail.com>
2024-11-24 10:34:21 +0530

    commit and push

--- src/components/ProductComp/ProductComp.tsx
+++ src/components/ProductComp/ProductComp.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
+import "./ProductComp.css";
 
 function ProductComp() {
   const images: string[] = [
     "https://lilypad-product-images.s3.ap-south-1.amazonaws.com/TRI_2912.png",
-    "https://lilypad-product-images.s3.ap-south-1.amazonaws.com/TRI_2912.png",
+    "https://lilypad-product-images.s3.ap-south-1.amazonaws.com/TRI_2909.png",
     "https://lilypad-product-images.s3.ap-south-1.amazonaws.com/TRI_2916.png",
     "https://lilypad-product-images.s3.ap-south-1.amazonaws.com/e-cycling%20in%20india.png",
   ];
@@ -11,27 +12,53 @@ function ProductComp() {
   const [selectedImg, setSelectedImg] = useState(images[0]);
 
   return (
-    <div className="w-full">
-        aa
+    <div className="w-[98%] mt-5 m-auto">
       <div className="flex">
-        <div className="flex-1 flex">
-          <div className="bg-red-500">
-            <ul >
-              {images.map((elm, key) => (
-                <li className="w-[50px] bg-red-500" key={key}>
-                  <img className="" src={elm} alt="lilyPad-list-img" />
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="flex-1">
-            <img src={selectedImg} alt="lilyPad-img" />
+        <div className="flex-1  ">
+          <div className="flex">
+            <div>
+              <ul>
+                {images.map((elm, key) => (
+                  <li
+                    className="w-[77px] rounded-xl flex items-center p-1 h-[77px] mb-4 border-1 shadow-custom"
+                    key={key}
+                  >
+                    <img
+                      className=""
+                      onMouseEnter={() => setSelectedImg(elm)}
+                      src={elm}
+                      alt="lilyPad-list-img"
+                    />
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="flex-1 ">
+              <div className="h-[350px] ">
+              <img
+                className=" h-[100%]  m-auto"
+                src={selectedImg}
+                alt="lilyPad-img"
+              />
+              </div>
+             
+              <div className="mt-5 w-[75%] m-auto">
+              <div className="flex gap-10">
+                <button className="buttonClass flex-1">Buy Now</button>
+                <button className="buttonClass flex-1">Book</button>
+              </div>
+              <p className="w-full text-center border-2 border-gray-600 text-2xl font-bold  mt-5 border-dashed p-2 ">
+                EMI @ 5000
+        
+              </p>
+              </div>
+              
+            </div>
           </div>
         </div>
         <div className="flex-1"></div>
       </div>
     </div>
- 
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#00FFFF", // Default primary color
+      },
+      boxShadow: {
+        custom:
+          "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;", // Custom shadow
+      },
+    },
   },
   plugins: [],
 };
