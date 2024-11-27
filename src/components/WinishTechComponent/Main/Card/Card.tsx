cb5419deb6d3b065160d26c324fc7076ee6b3b4c
Ashuajax <yesaj9354@gmail.com>
2024-11-27 21:47:10 +0530

    Commit And push

--- src/components/WinishTechComponent/Main/Card/Card.tsx
+++ src/components/WinishTechComponent/Main/Card/Card.tsx
@@ -10,8 +10,8 @@ function Card() {
     <div className="border-[3px] border-secondary rounded-2xl h-[max-content] p-4 shadow-bottom ">
       <div className="flex items-center">
         <div className="flex-1 flex items-center gap-2 ">
-          <h1 className="text-2xl font-bold text-[#003366]">East Auto</h1>{" "}
-          <button className="border-[2px] border-secondary rounded-full text-[12px] w-[50px] h-[25px] ">
+          <h1 className="text-2xl font-bold text-[#003366] ">East Auto</h1>{" "}
+          <button className="border-[3px] border-secondary rounded-full text-[12px] w-[50px] h-[25px] ">
             New
           </button>
         </div>{" "}
@@ -20,8 +20,8 @@ function Card() {
           <MoreVertIcon />
         </div>
       </div>
-      <h1 className="text-xl font-semibold mt-2">Patrick</h1>
-      <div className="flex justify-between mt-2">
+      <h1 className="text-xl font-semibold mt-1">Patrick</h1>
+      <div className="flex justify-between ">
         <p>+18323314168</p>
         <p>77018</p>
       </div>
@@ -36,7 +36,7 @@ function Card() {
           <p><SendIcon className="rotate-[-45deg] -mt-[12px]" /></p>
           
         </div>
-        <h1 className="text-xl font-bold text-[#003366]">#Notes</h1>{" "}
+        <h1 className="text-xl font-semibold text-[#003366]">#Notes</h1>{" "}
       </div>
     </div>
   );
